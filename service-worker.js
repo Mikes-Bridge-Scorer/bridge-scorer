3cb4e5c25a21f2521bfccedba26bcc35db01d1d2
Mikes-Bridge-Scorer <mike.chris.smith@gmail.com>
2024-12-07 13:40:27 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,17 @@
 const CACHE_NAME = 'bridge-scorer-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/app.js',
-  '/manifest.webmanifest'
+  '/bridge-scorer/',
+  '/bridge-scorer/index.html',
+  '/bridge-scorer/app.js',
+  '/bridge-scorer/manifest.json',
+  '/bridge-scorer/icons/icon-72x72.png',
+  '/bridge-scorer/icons/icon-96x96.png',
+  '/bridge-scorer/icons/icon-128x128.png',
+  '/bridge-scorer/icons/icon-144x144.png',
+  '/bridge-scorer/icons/icon-152x152.png',
+  '/bridge-scorer/icons/icon-192x192.png',
+  '/bridge-scorer/icons/icon-384x384.png',
+  '/bridge-scorer/icons/icon-512x512.png'
 ];
 
 // Install event
